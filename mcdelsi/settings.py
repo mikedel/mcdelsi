4891438615f17beb4e32e0cd7a7c22aac34ff96a
mikedel <mcdelsi@gmail.com>
2015-12-03 02:28:07 -0500

    ???

--- mcdelsi/settings.py
+++ mcdelsi/settings.py
@@ -145,5 +145,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = 'staticfiles'
 STATIC_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATICFILES_DIRS = (
-    os.path.join(STATIC_BASE_DIR, 'static'),
+    os.path.join(STATIC_BASE_DIR, 'chubbies/static'),
 )
