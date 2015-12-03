e04a4d5e5290fef923f5779e5ad041767e6c8c9c
mikedel <mcdelsi@gmail.com>
2015-12-03 02:34:30 -0500

    maybe

--- mcdelsi/settings.py
+++ mcdelsi/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/chubbies/static/'
 STATIC_ROOT = 'staticfiles'
 STATIC_BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATICFILES_DIRS = (
